0e935164370f0d7955ca46daea468b50e2ef3198
Tomek <tuteq13@gmail.com>
2020-08-26 16:23:15 +0200

    Filling first two cards with content

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = 0xFF1D1E33;
@@ -24,11 +25,49 @@ class _InputPageState extends State<InputPage> {
               children: [
                 Expanded(
                   child: ReusableCard(
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8d8e98),
+                          ),
+                        )
+                      ],
+                    ),
                     colour: Color(activeCardColour),
                   ),
                 ),
                 Expanded(
                   child: ReusableCard(
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        FaIcon(
+                          FontAwesomeIcons.venus,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'FEMALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8d8e98),
+                          ),
+                        )
+                      ],
+                    ),
                     colour: Color(activeCardColour),
                   ),
                 ),
